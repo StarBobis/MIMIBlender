212fbaadda594b2d5e05e74e7176202e72ff7b1b
Administrator <Administrator@DESKTOP-BBNRNSR>
2026-09-08 16:46:23 +0800

    版本更新

--- __init__.py
+++ __init__.py
@@ -34,7 +34,7 @@ bl_info = {
     "name": "MIMIBlender",
     "description": "The Blender add-on for MIMITools",
     "blender": (5, 2, 0),
-    "version": (1, 0, 5),
+    "version": (1, 0, 6),
     "location": "View3D",
     "category": "Generic"
 }
