808153e2fa143a46951e255acb0480cb34f324e9
Administrator <Administrator@DESKTOP-BBNRNSR>
2026-09-18 14:38:55 +0800

    chore(release): version 1.0.21

--- __init__.py
+++ __init__.py
@@ -52,7 +52,7 @@ bl_info = {
     "name": "MIMIBlender",
     "description": "The Blender add-on for MIMITools",
     "blender": (5, 2, 0),
-    "version": (1, 0, 20),
+    "version": (1, 0, 21),
     "location": "View3D",
     "category": "Generic"
 }
